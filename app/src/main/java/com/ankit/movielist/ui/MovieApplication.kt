fffb0e619845185fec4ee30b8e7b96d1e09c9c60
Ankit Garg <gargankit90@gmail.com>
2021-01-09 22:52:20 -0800

    Add Timber Debug Tree to Application

--- app/src/main/java/com/ankit/movielist/ui/MovieApplication.kt
+++ app/src/main/java/com/ankit/movielist/ui/MovieApplication.kt
@@ -1,8 +1,10 @@
 package com.ankit.movielist.ui
 
 import android.app.Application
+import com.ankit.movielist.BuildConfig
 import com.ankit.movielist.di.AppComponent
 import com.ankit.movielist.di.ComponentManager
+import timber.log.Timber
 
 /**
  * Created by Gauri Gadkari on 1/9/21.
@@ -15,5 +17,8 @@ class MovieApplication : Application() {
         super.onCreate()
         ComponentManager.application = this
         appComponent = ComponentManager.appComponent
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
     }
 }
