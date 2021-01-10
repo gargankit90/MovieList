4825be5b6340695ef5366efda5dd7762d643e09c
Ankit Garg <gargankit90@gmail.com>
2021-01-09 22:27:18 -0800

    Add custom application class

---
+++ app/src/main/java/com/ankit/movielist/ui/MovieApplication.kt
@@ -0,0 +1,14 @@
+package com.ankit.movielist.ui
+
+import android.app.Application
+
+/**
+ * Created by Gauri Gadkari on 1/9/21.
+ */
+class MovieApplication: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+
+}
