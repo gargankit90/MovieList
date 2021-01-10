89a75eb08c961a0a1eb78825534ec28516d47565
Ankit Garg <gargankit90@gmail.com>
2021-01-09 18:49:28 -0800

    Skeleton project with ktlint and detekt

---
+++ app/src/main/java/com/ankit/movielist/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.ankit.movielist
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
