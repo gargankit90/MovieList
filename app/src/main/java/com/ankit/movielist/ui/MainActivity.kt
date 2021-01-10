efa5700fffb5d689b21dbdc2013ecea821f6b171
Ankit Garg <gargankit90@gmail.com>
2021-01-09 22:27:32 -0800

    Move MainActivity to ui package

--- app/src/main/java/com/ankit/movielist/ui/MainActivity.kt
+++ app/src/main/java/com/ankit/movielist/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.ankit.movielist
+package com.ankit.movielist.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.ankit.movielist.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
