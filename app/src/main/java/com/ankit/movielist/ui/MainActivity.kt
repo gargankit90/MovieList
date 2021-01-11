0078c98a91066efa191857aeb58fc03b709469b0
Ankit Garg <gargankit90@gmail.com>
2021-01-10 16:15:51 -0800

    Add SearchFragment and PlayListFragment. Also add Bottom Navigation

--- app/src/main/java/com/ankit/movielist/ui/MainActivity.kt
+++ app/src/main/java/com/ankit/movielist/ui/MainActivity.kt
@@ -2,11 +2,45 @@ package com.ankit.movielist.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
 import com.ankit.movielist.R
+import com.ankit.movielist.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        navController = findNavController(R.id.navHostFragment)
+        setupBottomNav()
+    }
+
+    private fun setupBottomNav() {
+        binding.bottomNavigation.setOnNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.bottomNavigationSearchMenuId -> {
+                    navController
+                        .navigate(
+                            R.id.searchFragment
+                        )
+                    true
+                }
+                R.id.bottomNavigationPlayListMenuId -> {
+                    navController
+                        .navigate(
+                            R.id.playListFragment
+                        )
+                    true
+                }
+                else -> {
+                    false
+                }
+            }
+        }
     }
 }
